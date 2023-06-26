f365c40917ce53553816a51534c6250181c7cca3
Steven Gong <gonghaima@hotmail.com>
2023-06-26 16:22:04 +1000

    product page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import Home from './pages/Home';
+import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 
 function App() {
+  return <Product/>
   return <ProductList/>
   return <Home />;
 }
