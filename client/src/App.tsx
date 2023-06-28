81afdfe363466f779293c84817a122387191e58b
Steven Gong <gonghaima@hotmail.com>
2023-06-28 14:58:55 +1000

    cart page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import Cart from './pages/Cart';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Product from './pages/Product';
@@ -5,6 +6,7 @@ import ProductList from './pages/ProductList';
 import Register from './pages/Register';
 
 function App() {
+  return <Cart />;
   return <Login />;
   return <Register />;
   return <Product />;
