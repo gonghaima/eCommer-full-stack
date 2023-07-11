1567b8e9dd1b0efeb5f796b14ede775ff26fa4ff
Steven Gong <gonghaima@hotmail.com>
2023-07-11 18:54:54 +1000

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
 const mongoose = require('mongoose');
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('db connected!!!');
+    // app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(5000, () => {
   console.log('BE server is running!!');
