ba1ab31787e9ffaddc42570792cb00ca08e5da86
Steven Gong <gonghaima@hotmail.com>
2023-07-21 09:40:06 +1000

    add stripe

--- api/index.js
+++ api/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const mongoose = require('mongoose');
-const cors = require('cors');
 
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
+const cors = require('cors');
 
 dotenv.config();
 mongoose
@@ -36,6 +37,7 @@ app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 app.listen(5000, () => {
   console.log('BE server is running!!');
