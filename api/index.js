efad462a19f665b4f337045c598cb6e6afc5dc8f
Steven Gong <gonghaima@hotmail.com>
2023-07-20 11:23:31 +1000

    orders & cart api

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ const cors = require('cors');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 dotenv.config();
 mongoose
@@ -32,6 +34,8 @@ app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
 
 app.listen(5000, () => {
   console.log('BE server is running!!');
