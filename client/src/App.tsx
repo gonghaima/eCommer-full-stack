9a72a0aeb786e4d066732d73ecc55782eba43a3c
Steven Gong <gonghaima@hotmail.com>
2023-07-23 08:13:28 +1000

    add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import Cart from './pages/Cart';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -5,13 +7,35 @@ import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 import Register from './pages/Register';
 
+function RedirectHome() {
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    navigate('/');
+  }, [navigate]);
+
+  return null;
+}
+
 function App() {
-  return <Home />;
-  return <Login />;
-  return <Cart />;
-  return <Product />;
-  return <ProductList />;
-  return <Register />;
+  const user = true;
+
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:category" element={<ProductList />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={user ? <RedirectHome /> : <Login />} />
+          <Route
+            path="/register"
+            element={user ? <RedirectHome /> : <Register />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
 }
 
 export default App;
