bc087b15dc6183a69b2208c213afeed79d03adfc
Steven Gong <gonghaima@hotmail.com>
2023-07-25 21:03:44 +1000

    set filter and sort with server data

--- client/src/components/Products.tsx
+++ client/src/components/Products.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { popularProducts } from '../data';
 import Product from './Product';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Container = styled.div`
   padding: 20px;
@@ -9,11 +11,68 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-function Products() {
+function Products({ cat, filters, sort }) {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:5000/api/products?category=${cat}`
+            : 'http://localhost:5000/api/products'
+        );
+        setProducts(res.data);
+      } catch (error) {}
+    };
+    getProducts();
+  }, [cat]);
+
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
+
+  useEffect(() => {
+    if (sort === 'newest') {
+      console.log('set newest...');
+
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === 'asc') {
+      console.log('set asc...');
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      console.log('set dsc...');
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   return (
     <Container>
-      {popularProducts.map((item) => (
-        <Product item={item} key={item.id} />
+      {(cat ? filteredProducts : products).map((item) => (
+        <Product
+          item={item}
+          key={item.id ?? getRandomInt(10, 10000)}
+          idx={item.id ?? getRandomInt(10, 10000)}
+        />
       ))}
     </Container>
   );
