936979db76461d5a4d9209aa93cf81e4a033977b
Steven Gong <gonghaima@hotmail.com>
2023-08-12 09:45:05 +1000

    side bar

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
-    <>
+    <Router>
       <Topbar />
-    </>
+      <div className="container">
+        <Sidebar />
+      </div>
+    </Router>
   );
 }
 
