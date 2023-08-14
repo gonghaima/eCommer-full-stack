abe4396334540b613380b1572475488097db342d
Steven Gong <gonghaima@hotmail.com>
2023-08-14 17:47:30 +1000

    add chart

--- admin/src/pages/home/home.tsx
+++ admin/src/pages/home/home.tsx
@@ -1,10 +1,18 @@
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import './home.css';
+import { userData } from '../../dummyData';
+import Chart from '../../components/chart/Chart';
 
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 }
