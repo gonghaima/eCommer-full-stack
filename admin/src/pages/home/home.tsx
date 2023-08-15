3d501006cba303593bb967a97c9bcb6d1fe334f4
Steven Gong <gonghaima@hotmail.com>
2023-08-15 18:02:53 +1000

    add widget

--- admin/src/pages/home/home.tsx
+++ admin/src/pages/home/home.tsx
@@ -2,6 +2,8 @@ import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import './home.css';
 import { userData } from '../../dummyData';
 import Chart from '../../components/chart/Chart';
+import WidgetSm from '../../components/widgetSm/WidgetSm';
+import WidgetLg from '../../components/widgetLg/WidgetLg';
 
 export default function Home() {
   return (
@@ -13,6 +15,10 @@ export default function Home() {
         grid
         dataKey="Active User"
       />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   );
 }
